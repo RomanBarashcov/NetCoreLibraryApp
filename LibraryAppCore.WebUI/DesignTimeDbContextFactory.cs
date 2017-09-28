614a33d6e69bb0b8c8f7ab6fed1437dc6c629b67
Roman Barashkov <romanderson94@gmail.com>
2017-09-28 19:13:39 +0300

    add helper for migrations , and ef core tolls

---
+++ LibraryAppCore.WebUI/DesignTimeDbContextFactory.cs
@@ -0,0 +1,31 @@
+﻿using LibraryAppCore.Domain.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryAppCore.WebUI
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<LibraryPostgreSqlContext>
+    {
+        public LibraryPostgreSqlContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<LibraryPostgreSqlContext>();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            builder.UseNpgsql(connectionString, b => b.MigrationsAssembly("LibraryAppCore.WebUI"));
+
+            return new LibraryPostgreSqlContext(builder.Options);
+        }
+    }
+}
